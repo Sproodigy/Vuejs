c53ec385a89c44c950c0ea054dd23442e3b41099
Sproodigy <sproodigy@gmail.com>
2017-12-05 21:54:25 +0400

    Just practice

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,45 @@
 <template>
   <div id="app">
-     <head>
-      <meta charset="utf-8">
-      <title>align-content</title>
-     </head>
-     <body>
-      <div class="flex-cont">
-       <div class="yellow"></div>
-    </div>
-      <div class="flex-contain">
-       <div class="red"></div>
-       <div class="yellow"></div>
-       <div class="green"></div>
-      </div>
-     </body>
-  </div>
+<head>
+<meta charset="utf-8">
+<title>all</title>
+</head>
+<body>
+   <div class="creature"></div>
+ </body>
+</div>
 </template>
 
 <style>
- .flex-contain {
-  width: 100px;
-  height: 290px;
-  border: 2px solid #333;
-  margin: 30px;
-  padding: 30px;
-  /*display: flex;*/
-  /*flex-wrap: wrap;*/
-  align-content: center;
- }
-
- .flex-contain div {
-  width: 90px; height: 90px;
-  border-radius: 90%;
- }
-
- .flex-cont div {
-  width: 50px; height: 50px;
-  border-radius: 40%;
- }
-
-  .flex-cont {
-    padding: 30px;
-    background: blue;
-    flex-wrap: flex;
-    display: flex;
-    align-content: flex-end;
-    width: 190px; height: 90px;
-    border: 4pt dotted yellow;
-    border-radius: 40%;
- }
-
- .red { background: magenta; }
- .yellow { background: yellow; }
- .green { background: green; }
-</style>
-
+   .creature {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background: #3ac;
+    position: relative;
+   }
+   .creature::before, .creature::after {
+    content: '';
+    position: absolute;
+    width: 30px;
+    height: 40px;
+    border-radius: 50%;
+    background: #000;
+    top: 40px;
+    animation: eye 3s ease-in-out infinite;
+    -webkit-animation: eye 3s ease-in-out infinite;
+   }
+   .creature::before { left: 55px; }
+   .creature::after { right: 55px; }
+   @keyframes eye {
+    90% { transform: none; }
+    95% { transform: scaleY(0.1); }
+   }
+   @-webkit-keyframes eye {
+    90% { transform: none; }
+    95% { transform: scaleY(0.1); }
+   }
+  </style>
 <!-- // <script> -->
 // export default {
 //   name: 'app',
